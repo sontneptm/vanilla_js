784fb310153fa6f02d0c89a460da5736035dc987
Seungmin Jeong <sontneptm2@gmail.com>
2022-03-02 17:43:32 +0900

    update

--- app.js
+++ app.js
@@ -1,23 +1,4 @@
-const calculator ={
-    add: function(a, b){
-        console.log(String(a)+ " + " +String(b) + " = " +String(a+b))
-    },
-    abs: function(a, b){
-        console.log(String(a)+ " - " +String(b) + " = " +String(a-b))
-    },
-    mul: function(a, b){
-        console.log(String(a)+ " x " +String(b) + " = " +String(a*b))
-    },
-    div: function(a, b){
-        console.log(String(a)+ " / " +String(b) + " = " +String(a/b))
-    },
-    power: function(a, b){
-        console.log(String(a)+ " power of " +String(b) + " = " +String(a**b))
-    },
-}
+let age = prompt("how old are u?");
+age = parseInt(age);
 
-calculator.add(5,1);
-calculator.abs(3,6);
-calculator.mul(55,13);
-calculator.div(5,3);
-calculator.power(5,2);
+console.log(typeof age);
